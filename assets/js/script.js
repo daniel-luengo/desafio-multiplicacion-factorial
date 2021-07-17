116a9e7a317a531f31e84de98bbba33dbff9e1ff
daniel-luengo <daniel.luengo.v@gmail.com>
2021-07-17 19:54:12 -0400

    Hemos pasado todo a una funcion

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,33 @@
 
 
-let valorSeleccionado =parseInt(prompt("Ingrese un numero de 1 a 20"));
+
+
+
+
+function solicitarNumero() {
+    let valorSeleccionado =parseInt(prompt("Ingrese un numero de 1 a 20"));
 
     if( (valorSeleccionado>=1) && (valorSeleccionado <=20)) {
         alert("entramos")
     }  else {
         alert("número fuera de rango")
     }
+}
 
+solicitarNumero();
 
 
  
+/*
+var eleccion =3;
+function factorial(){
+
+    for(var i=1; i<=eleccion; i++) {
+
+        var resultado = i*i+1;
+        alert (resultado)
+
+    }
+
+}
+factorial()*/
