eaa05d3e6b60707d301caf76474871858c206253
daniel-luengo <daniel.luengo.v@gmail.com>
2021-07-17 17:12:55 -0400

    Validacion de rango de numero permitido para ingresar al programa listo

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+
+
+let valorSeleccionado =parseInt(prompt("Ingrese un numero de 1 a 20"));
+
+    if( (valorSeleccionado>=1) && (valorSeleccionado <=20)) {
+        alert("entramos")
+    }  else {
+        alert("número fuera de rango")
+    }
+
+
+
+ 
